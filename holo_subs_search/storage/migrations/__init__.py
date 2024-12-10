2581f1db740a13456607bc404bf8d004f7169bdc
Jiří Kuneš <jirka642@gmail.com>
2024-12-11 00:49:42 +0100

    [FIX] Fixed bad import

--- holo_subs_search/storage/migrations/__init__.py
+++ holo_subs_search/storage/migrations/__init__.py
@@ -1,3 +1,3 @@
-from m_0_1_0 import migrate_0_1_0
-from m_0_2_0 import migrate_0_2_0
-from m_0_3_0 import migrate_0_3_0
+from .m_0_1_0 import migrate_0_1_0
+from .m_0_2_0 import migrate_0_2_0
+from .m_0_3_0 import migrate_0_3_0
