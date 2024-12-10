6f337be740f04f5914a1cf44bd993fde93a7f652
Jiří Kuneš <jirka642@gmail.com>
2024-12-11 00:18:16 +0100

    [IMP] Migration of storage to 0.4.0 format

--- holo_subs_search/storage/storage.py
+++ holo_subs_search/storage/storage.py
@@ -4,17 +4,19 @@ import json
 import logging
 import os
 import pathlib
+import re
 import weakref
-from typing import Any, Callable, Iterator, TypeVar
+from typing import Any, Callable, Iterator, Literal, TypeVar
 
 from .. import __version__
-from ..utils import json_dumps
+from ..utils import get_checksum, json_dumps
 from .channel import ChannelRecord
 from .mixins.metadata_mixin import MetadataMixin
 from .record import Record
 from .video import VideoRecord
 
 RecordT = TypeVar("RecordT", bound=Record)
+GitPrivacyType = Literal["private", "public"]
 
 _logger = logging.getLogger(__name__)
 
@@ -38,8 +40,8 @@ class Storage(MetadataMixin):
         return self.path
 
     @classmethod
-    def build_metadata(cls, **kwargs) -> dict[str, Any]:
-        return super().build_metadata(**kwargs) | {"version": __version__}
+    def build_metadata(cls, git_privacy: GitPrivacyType = "private", **kwargs) -> dict[str, Any]:
+        return super().build_metadata(**kwargs) | {"version": __version__, "git_privacy": git_privacy}
 
     def migrate(self) -> None:
         visited = set()
@@ -51,6 +53,7 @@ class Storage(MetadataMixin):
 
             self.migrate_0_1_0()
             self.migrate_0_2_0()
+            self.migrate_0_3_0()
 
             if version == self.metadata["version"]:
                 break
@@ -170,6 +173,70 @@ class Storage(MetadataMixin):
         # storage format had only minor changes that should be compatible
 
         self.metadata = dict(self.metadata, version="0.3.0")
+
+    def migrate_0_3_0(self) -> None:
+        if self.metadata["version"] != "0.3.0":
+            return
+        _logger.info("Storage migration from version 0.3.0")
+
+        # storage could only have "public" git_privacy in the past
+        self.metadata = dict(self.metadata, git_privacy="public")
+
+        # convert content_id of content items to new format
+        video_table_p = self.path / "video"
+        if video_table_p.exists() and video_table_p.is_dir():
+            self._migrate_0_3_0__video_table(video_table_p)
+
+        self.metadata = dict(self.metadata, version="0.4.0")
+
+    def _migrate_0_3_0__video_table(self, video_table_p: pathlib.Path) -> None:
+        with os.scandir(video_table_p) as it:
+            for entry in it:
+                video_entry_p = video_table_p / entry.name
+                if video_entry_p.exists() and video_entry_p.is_dir() and (video_entry_p / "metadata.json").exists():
+                    self._migrate_0_3_0__video(video_entry_p)
+
+    def _migrate_0_3_0__video(self, video_p: pathlib.Path) -> None:
+        content_p = video_p / "content"
+        if content_p.exists() and content_p.is_dir():
+            with os.scandir(content_p) as it:
+                for entry in it:
+                    item_p = content_p / entry.name
+                    if item_p.exists() and item_p.is_dir() and (item_p / "metadata.json").exists():
+                        self._migrate_0_3_0__content_item(item_p)
+
+    def _migrate_0_3_0__content_item(self, item_p: pathlib.Path) -> None:
+        item_metadata = json.loads((item_p / "metadata.json").read_text())
+        item_type = item_metadata["item_type"]
+
+        if item_type == "subtitle":
+            name = item_metadata["subtitle_file"]
+            content = (item_p / name).read_bytes()
+            new_content_id = self._migrate_0_3_0__build_content_id(
+                item_type, item_metadata["source"], get_checksum(content), name
+            )
+        elif item_type == "audio":
+            name = item_metadata["audio_file"]
+            content = (item_p / name).read_bytes()
+            new_content_id = self._migrate_0_3_0__build_content_id(
+                item_type, item_metadata["source"], get_checksum(content), name
+            )
+        else:
+            _logger.warning("Could not fix content_id of: %s", item_p)
+            return
+
+        new_item_p = item_p.parent / new_content_id
+        _logger.info("Rename: %s -> %s", item_p, new_item_p)
+        item_p.rename(new_item_p)
+
+    def _migrate_0_3_0__build_content_id(self, item_type: str, *parts: Any) -> str:
+        if not parts:
+            raise ValueError("At least one content ID part is required")
+
+        parts = [str(x) for x in [item_type, *parts]]
+        parts = [re.sub(r"[^a-zA-Z0-9\-]+", "-", x) for x in parts]
+
+        return "_".join(parts)
 
     # Generic Records
 
