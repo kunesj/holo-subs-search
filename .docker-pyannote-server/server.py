dbe2446dcd5c0c223f4f017e07b4abad3cdac48a
Jiří Kuneš <jirka642@gmail.com>
2024-12-13 01:46:44 +0100

    [FIX] Use the same embedding model for returned speaker embeddings as for the diarization

--- .docker-pyannote-server/server.py
+++ .docker-pyannote-server/server.py
@@ -9,85 +9,64 @@ import numpy
 import pyannote.audio
 import pydub
 import torch
+import torchaudio
 from fastapi import FastAPI, HTTPException, Query, Response, UploadFile
+from pyannote.audio.pipelines.speaker_diarization import SpeakerDiarization
 
 _logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 CACHE: dict[str, Any] = {
-    "diarization_pipeline": None,
-    "diarization_model": None,
-    "embedding_pipeline": None,
-    "embedding_model": None,
+    "pipeline": None,
+    "checkpoint": None,
 }
 TORCH_DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 _logger.info("TORCH_DEVICE: %s", TORCH_DEVICE)
 
 
-def load_diarization_pipeline(model: str, huggingface_token: str | None) -> pyannote.audio.Pipeline:
-    if model == CACHE["diarization_model"]:
-        _logger.info("Using cached diarization model pipeline: %s", model)
-        pipeline = CACHE["diarization_pipeline"]
+def load_diarization_pipeline(*, checkpoint: str, huggingface_token: str | None) -> SpeakerDiarization:
+    if checkpoint == CACHE["checkpoint"]:
+        _logger.info("Using cached diarization model pipeline: %s", checkpoint)
+        pipeline = CACHE["pipeline"]
 
     else:
-        _logger.info("Loading diarization model pipeline: %s", model)
-        CACHE["diarization_pipeline"] = None
-        CACHE["diarization_model"] = None
+        _logger.info("Loading diarization model pipeline: %s", checkpoint)
+        CACHE.update(
+            {
+                "pipeline": None,
+                "checkpoint": None,
+            }
+        )
 
         pipeline = pyannote.audio.Pipeline.from_pretrained(
-            checkpoint_path=model,
+            checkpoint_path=checkpoint,
             use_auth_token=huggingface_token,
         )
 
         if not pipeline:
             raise HTTPException(
-                status_code=500,
+                status_code=400,
                 detail=(
-                    f"Diarization Model could not be loaded! "
+                    f"Diarization checkpoint could not be loaded! "
                     f"huggingface_token {huggingface_token!r} might be missing, invalid, "
-                    f"or user conditions of the model {model!r} were not accepted yet."
+                    f"or user conditions of the model {checkpoint!r} were not accepted yet."
                 ),
+            )
+        elif not isinstance(pipeline, SpeakerDiarization):
+            raise HTTPException(
+                status_code=400,
+                detail="Unexpected pipeline loaded!",
             )
 
         _logger.info("Moving diarization model to: %s", TORCH_DEVICE)
         pipeline.to(TORCH_DEVICE)
 
-        CACHE["diarization_pipeline"] = pipeline
-        CACHE["diarization_model"] = model
-
-    return pipeline
-
-
-def load_embedding_pipeline(model: str, huggingface_token: str | None) -> pyannote.audio.Pipeline:
-    if model == CACHE["embedding_model"]:
-        _logger.info("Using cached embedding model pipeline: %s", model)
-        pipeline = CACHE["embedding_pipeline"]
-
-    else:
-        _logger.info("Loading embedding model pipeline: %s", model)
-        CACHE["embedding_pipeline"] = None
-        CACHE["embedding_model"] = None
-
-        pipeline = pyannote.audio.pipelines.speaker_verification.PretrainedSpeakerEmbedding(
-            embedding="speechbrain/spkrec-ecapa-voxceleb",
-            use_auth_token=huggingface_token,
+        CACHE.update(
+            {
+                "pipeline": pipeline,
+                "checkpoint": checkpoint,
+            }
         )
-
-        if not pipeline:
-            raise HTTPException(
-                status_code=500,
-                detail=(
-                    f"Embedding Model could not be loaded! "
-                    f"huggingface_token {huggingface_token!r} might be missing, invalid, "
-                    f"or user conditions of the model {model!r} were not accepted yet."
-                ),
-            )
-
-        _logger.info("Moving embedding model to: %s", TORCH_DEVICE)
-        pipeline.to(TORCH_DEVICE)
-
-        CACHE["embedding_pipeline"] = pipeline
-        CACHE["embedding_model"] = model
 
     return pipeline
 
@@ -117,9 +96,16 @@ class DiarizationSegment:
 
 @dataclasses.dataclass
 class DiarizationResponse:
-    diarization_model: str
-    diarization: list[DiarizationSegment]
-    embedding_model: str
+    # configuration
+    checkpoint: str
+    segmentation_model: str | None
+    segmentation_batch_size: int
+    embedding_model: str | None
+    embedding_batch_size: int
+    embedding_exclude_overlap: bool
+    clustering: str
+    # result
+    segments: list[DiarizationSegment]
     embeddings: dict[str, list[float]]
 
 
@@ -132,12 +118,12 @@ def healthcheck() -> Response:
 
 
 # Can be tested with:
-# curl -X POST -F "file=@/home/????/????/599.m4a" "http://0.0.0.0:8001/diarization?huggingface_token=????"
+# curl -X POST -F "file=@/home/????/????/599.m4a"
+# "http://0.0.0.0:8001/diarization?checkpoint=pyannote/speaker-diarization-3.1&huggingface_token=????"
 @app.post("/diarization")
 async def diarization(
     file: UploadFile,
-    diarization_model: Annotated[str, Query()],
-    embedding_model: Annotated[str, Query()],
+    checkpoint: Annotated[str, Query()],
     huggingface_token: Annotated[str | None, Query()] = None,
 ) -> DiarizationResponse:
     """
@@ -146,15 +132,12 @@ async def diarization(
         - Supports all audio formats that are supported by `pydub`, but using `wav` will be faster,
           because the file will not have to be converted into it.
 
-        diarization_model:
+        checkpoint:
         - Accept https://hf.co/pyannote/segmentation-3.0 user conditions
         - Accept https://hf.co/pyannote/speaker-diarization-3.1 user conditions
-        - Use: pyannote/speaker-diarization-3.1
+        - Use: `pyannote/speaker-diarization-3.1`
 
-        embedding_model:
-        - Use: speechbrain/spkrec-ecapa-voxceleb
-
-        embedding_model:
+        huggingface_token:
         - Create access token at https://hf.co/settings/tokens and use it when calling this endpoint
 
     Probably doesn't handle switching between models or concurrent requests very well.
@@ -166,71 +149,37 @@ async def diarization(
     >>> embedding2: list[float]
     >>> distance = cdist(embedding1, embedding2, metric="cosine")
     """
-    diarization_pipeline = load_diarization_pipeline(diarization_model, huggingface_token)
-    embedding_pipeline = load_embedding_pipeline(embedding_model, huggingface_token)
-    wav_stream = await file_to_wav_stream(file)
+    pipeline = load_diarization_pipeline(checkpoint=checkpoint, huggingface_token=huggingface_token)
 
-    # detect speakers
-    # TODO: maybe check this for better results?
-    #   https://github.com/pyannote/pyannote-audio/blob/develop/tutorials/community/offline_usage_speaker_diarization.ipynb
-    #   https://github.com/wenet-e2e/wespeaker
+    # preload audio into memory for faster processing
+    waveform, sample_rate = torchaudio.load(await file_to_wav_stream(file))
 
     _logger.info("Diarizating audio file...")
+    diarization, raw_embeddings = pipeline({"waveform": waveform, "sample_rate": sample_rate}, return_embeddings=True)
+
+    _logger.info("Reading segments...")
     segments = [
         DiarizationSegment(start=turn.start, end=turn.end, speaker=speaker)
-        for turn, _, speaker in diarization_pipeline(wav_stream).itertracks(yield_label=True)
+        for turn, _, speaker in diarization.itertracks(yield_label=True)
     ]
 
-    # compute speaker embeddings
-    # based on https://github.com/pyannote/pyannote-audio/blob/develop/tutorials/speaker_verification.ipynb
-
-    _logger.info("Calculating embeddings...")
-
-    audio = pyannote.audio.Audio(sample_rate=16000, mono="downmix")
-    embeddings: dict[str, list[float]] = {}
-
-    for speaker in {x.speaker for x in segments}:
-        # get embedding for every segment with the speaker
-
-        speaker_embeddings: list[numpy.ndarray] = []
-
-        for segment in segments:
-            if segment.speaker != speaker:
-                continue
-            elif (segment.end - segment.start) < 0.1:
-                # embedding generation usually fails for tiny segments
-                continue
-
-            waveform, sample_rate = audio.crop(
-                # some diarization segments might incorrectly end tiny bit AFTER the end of audio,
-                # so we use mode="pad" to fill the missing time with zeros
-                wav_stream,
-                pyannote.core.Segment(segment.start, segment.end),
-                mode="pad",
-            )
-            embedding = embedding_pipeline(waveform[None]).flatten()
-
-            if numpy.any(numpy.isnan(embedding)):
-                _logger.warning("Embedding not generated: %s: %s -> %s", speaker, segment.start, segment.end)
-                continue
-
-            speaker_embeddings.append(embedding)
-
-        if not speaker_embeddings:
-            _logger.error("No speaker embeddings generated: %s", speaker)
+    _logger.info("Reading embeddings...")
+    embeddings = {}
+    for idx in range(raw_embeddings.shape[0]):
+        if numpy.any(numpy.isnan(raw_embeddings[idx, :])):
             continue
-
-        # calculate mean embedding to fully describe the speaker with just one value
-        # - https://www.reddit.com/r/LanguageTechnology/comments/st1si5/averaging_sentence_embeddings_to_create/
-        # - https://en.wikipedia.org/wiki/Kernel_embedding_of_distributions
-        # - https://github.com/RF5/simple-speaker-embedding
-
-        mean_embedding = numpy.mean(speaker_embeddings, axis=0)
-        embeddings[speaker] = mean_embedding.tolist()
+        embeddings[f"SPEAKER_{idx:0>2}"] = raw_embeddings[idx, :].flatten().tolist()
 
     return DiarizationResponse(
-        diarization_model=diarization_model,
-        diarization=segments,
-        embedding_model=embedding_model,
+        # config
+        checkpoint=checkpoint,
+        segmentation_model=pipeline.segmentation_model if isinstance(pipeline.segmentation_model, str) else None,
+        segmentation_batch_size=pipeline.segmentation_batch_size,
+        embedding_model=pipeline.embedding if isinstance(pipeline.embedding, str) else None,
+        embedding_batch_size=pipeline.embedding_batch_size,
+        embedding_exclude_overlap=pipeline.embedding_exclude_overlap,
+        clustering=pipeline.klustering,
+        # results
+        segments=segments,
         embeddings=embeddings,
     )
