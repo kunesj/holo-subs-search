1920caa6918d89f0c0e0298d3497e29550791668
Jiří Kuneš <jirka642@gmail.com>
2024-12-17 03:33:10 +0100

    [FIX] Handle HTTP 429 from Holodex; Always download full channel info from Holodex

--- holo_subs_search/holodex_tools.py
+++ holo_subs_search/holodex_tools.py
@@ -2,8 +2,10 @@ from __future__ import annotations
 
 import asyncio
 import logging
-from typing import AsyncIterator, Iterable
+from types import TracebackType
+from typing import Any, AsyncIterator, Iterable, Literal, Optional, Self
 
+import aiohttp
 from holodex.client import HolodexClient
 from holodex.model.channel import Channel
 from holodex.model.channel_video import ChannelVideoInfo
@@ -12,12 +14,59 @@ from holodex.model.channels import LiteChannel
 from .env_config import HOLODEX_API_KEY, HOLODEX_PARALLEL_COUNT
 
 _logger = logging.getLogger(__name__)
-HOLODEX_SEMAPHORE = asyncio.Semaphore(HOLODEX_PARALLEL_COUNT)
+
+
+class BetterHolodexClient(HolodexClient):
+    HOLODEX_SEMAPHORE = asyncio.Semaphore(HOLODEX_PARALLEL_COUNT)
+    MAX_RETRY = 7  # 128 seconds
+
+    async def __aenter__(self) -> Self:
+        await self.HOLODEX_SEMAPHORE.acquire()
+        return await super().__aenter__()
+
+    async def __aexit__(
+        self,
+        exc_type: Optional[type[BaseException]],
+        exc_val: Optional[BaseException],
+        exc_tb: Optional[TracebackType],
+    ) -> None:
+        try:
+            return await super().__aexit__(exc_type=exc_type, exc_val=exc_val, exc_tb=exc_tb)
+        finally:
+            self.HOLODEX_SEMAPHORE.release()
+
+    async def request(
+        self,
+        method: Literal["GET", "POST"],
+        endpoint: str,
+        **kwargs: Any,
+    ) -> Any:
+        if not self.session:
+            self.session = aiohttp.ClientSession()
+
+        http_429_count = 0
+        while True:
+            async with self.session.request(
+                method,
+                self.BASE_URL + endpoint,
+                headers=self.headers,
+                **kwargs,
+            ) as response:
+                if response.status == 429 and http_429_count > self.MAX_RETRY:
+                    raise
+                elif response.status == 429:
+                    _logger.info("Got HTTP 429, will retry in %s seconds", 2**http_429_count)
+                    await asyncio.sleep(2**http_429_count)
+                    http_429_count += 1
+                    continue
+
+                response.raise_for_status()
+                return await response.json()
 
 
 async def download_channel_video_info(channel_ids: Iterable[str]) -> AsyncIterator[ChannelVideoInfo]:
     limit = 50
-    async with HOLODEX_SEMAPHORE, HolodexClient(key=HOLODEX_API_KEY) as client:
+    async with BetterHolodexClient(key=HOLODEX_API_KEY) as client:
         for channel_id in channel_ids:
             for video_type in ["videos", "collabs"]:
                 offset = 0
@@ -40,7 +89,7 @@ async def download_channel_video_info(channel_ids: Iterable[str]) -> AsyncIterat
 
 async def download_org_channels(org: str) -> AsyncIterator[LiteChannel]:
     limit = 50
-    async with HOLODEX_SEMAPHORE, HolodexClient(key=HOLODEX_API_KEY) as client:
+    async with BetterHolodexClient(key=HOLODEX_API_KEY) as client:
         offset = 0
         while True:
             # noinspection PyTypeChecker
@@ -51,7 +100,8 @@ async def download_org_channels(org: str) -> AsyncIterator[LiteChannel]:
                 offset=offset,
             )
             for lite_channel in channels:
-                yield lite_channel
+                # we want the full channel info, the lite channel is missing some important information
+                yield await client.channel(lite_channel.id)
 
             offset += len(channels)
             if len(channels) < limit:
@@ -59,7 +109,7 @@ async def download_org_channels(org: str) -> AsyncIterator[LiteChannel]:
 
 
 async def download_channels(channel_ids: Iterable[str]) -> AsyncIterator[Channel]:
-    async with HOLODEX_SEMAPHORE, HolodexClient(key=HOLODEX_API_KEY) as client:
+    async with BetterHolodexClient(key=HOLODEX_API_KEY) as client:
         for channel_id in channel_ids:
             try:
                 yield await client.channel(channel_id)
