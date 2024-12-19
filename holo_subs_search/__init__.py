bca890fc01ebc93a7281db32499a0f271e0e75a8
Jiří Kuneš <jirka642@gmail.com>
2024-12-19 01:53:09 +0100

    [VER] Bumped version to 0.9.0

--- holo_subs_search/__init__.py
+++ holo_subs_search/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "0.8.0"
+__version__ = "0.9.0"
 # storage format is versioned separately, so that we don't have to create migrations when it's not changed
 __storage_version__ = "0.7.0"
